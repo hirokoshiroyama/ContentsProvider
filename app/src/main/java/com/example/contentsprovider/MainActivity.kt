123a301fa10418a0ce8d28dbf0f3f8e4cfbc534e
hirokoshiroyama <inai.inai.baa777@gmail.com>
2020-07-18 16:52:31 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/contentsprovider/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.contentsprovider
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.provider.FontRequest
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }else{
+                    requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),PERMISSIONS_REQUEST_CODE)
+                }
+            }else{
+             getContentsInfo()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode:Int,permission:Array<String>,grantResults: IntArray){
+        when(requestCode){
+            PERMISSIONS_REQUEST_CODE->
+                if (grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+                    getContentsInfo()
+                }
+        }
+    }
+    private fun getContentsInfo(){
+        val resolver = contentResolver
+        val cursor =resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+        null
+        )
+
+        if (cursor!!.moveToFirst()){
+            do{
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
+
+                Log.d("ANDROID","URI:"+imageUri.toString())
+            }while (cursor.moveToNext())
+        }
+        cursor.close()
+    }
+
+}
+
