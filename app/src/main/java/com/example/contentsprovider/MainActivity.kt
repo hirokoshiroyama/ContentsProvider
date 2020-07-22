cd4c04b06635b7e6f84be0ed7e23ecf187a8d3b6
hirokoshiroyama <inai.inai.baa777@gmail.com>
2020-07-22 23:09:30 +0900

    終了

--- app/src/main/java/com/example/contentsprovider/MainActivity.kt
+++ app/src/main/java/com/example/contentsprovider/MainActivity.kt
@@ -5,10 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.PackageManager
 import android.os.Build
-import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
-import android.provider.FontRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,43 +17,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED) {
-                    getContentsInfo()
-                }else{
-                    requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),PERMISSIONS_REQUEST_CODE)
-                }
-            }else{
-             getContentsInfo()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                getContentsInfo()
+            } else {
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+        } else {
+            getContentsInfo()
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode:Int,permission:Array<String>,grantResults: IntArray){
-        when(requestCode){
-            PERMISSIONS_REQUEST_CODE->
-                if (grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permission: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContentsInfo()
                 }
         }
     }
-    private fun getContentsInfo(){
+
+    private fun getContentsInfo() {
         val resolver = contentResolver
-        val cursor =resolver.query(
+        val cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             null,
             null,
             null,
-        null
+            null
         )
 
-        if (cursor!!.moveToFirst()){
-            do{
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
-
-                Log.d("ANDROID","URI:"+imageUri.toString())
-            }while (cursor.moveToNext())
+        if (cursor!!.moveToFirst()) {
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            imageView.setImageURI(imageUri)
         }
         cursor.close()
     }
